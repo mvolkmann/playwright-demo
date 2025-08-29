74732ab0d10f1654f3d47c76b1886eec0fd29067
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-08-29 13:22:21 -0500

    changed components to use TypeScript

--- src/traffic-light.ts
+++ src/traffic-light.ts
@@ -13,12 +13,13 @@ template.innerHTML = html`
       padding: 0.5rem;
     }
     div {
+      --size: 3rem;
       border: none;
       border-radius: 50%;
       font-weight: bold;
       padding: 0.5rem;
-      height: 3rem;
-      width: 3rem;
+      height: var(--size);
+      width: var(--size);
 
       opacity: 0.4;
       &.on {
@@ -51,11 +52,12 @@ template.innerHTML = html`
  * To get the current state, access the `state` property of an instance.
  * It is intentional that the state cannot be changed by
  * modifying the `state` attribute or setting the `state` property.
+ * @attr {string} [state="stop"] - The starting state.
  */
-class TrafficLight extends HTMLElement {
-  static states = ["stop", "yield", "go"];
-  #state;
-  #stateToDivMap = new Map();
+export default class TrafficLight extends HTMLElement {
+  private static states = ["stop", "yield", "go"];
+  private _state: string = "stop";
+  private stateToDivMap = new Map();
 
   constructor() {
     super();
@@ -63,41 +65,48 @@ class TrafficLight extends HTMLElement {
   }
 
   connectedCallback() {
-    const initial = this.getAttribute("state");
-    this.#state = TrafficLight.states.includes(initial) ? initial : "stop";
+    const initial = this.getAttribute("state") ?? "";
+    this._state = TrafficLight.states.includes(initial) ? initial : "stop";
 
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot?.appendChild(template.content.cloneNode(true));
 
-    const divs = this.shadowRoot.querySelectorAll("div");
+    const divs = this.shadowRoot?.querySelectorAll("div") ?? [];
     TrafficLight.states.forEach((state, index) => {
-      this.#stateToDivMap.set(state, divs[index]);
+      this.stateToDivMap.set(state, divs[index]);
     });
 
-    const button = this.shadowRoot.querySelector("button");
-    button.addEventListener("click", () => this.next());
-    this.#change(true);
+    // Listen on the custom element instead of the button
+    // so the click method can be called on it.
+    this.addEventListener("click", () => this.next());
+    this.change(true);
   }
 
   get state() {
-    return this.#state;
+    return this._state;
   }
 
   next() {
-    this.#change(false);
-    const s = this.#state;
-    this.#state = s === "stop" ? "yield" : s === "yield" ? "go" : "stop";
-    this.#change(true);
+    this.change(false);
+    const s = this.state;
+    this._state = s === "stop" ? "yield" : s === "yield" ? "go" : "stop";
+    this.change(true);
     this.dispatchEvent(
-      new CustomEvent("stateChanged", {
-        detail: { state: this.#state },
+      new CustomEvent("state-change", {
+        detail: { state: this.state },
         bubbles: true,
         composed: true,
       })
     );
+    /*
+    console.log(
+      'traffic-light.js "next": dispatched state-change for',
+      this.#state
+    );
+    */
   }
 
-  #change(on) {
-    this.#stateToDivMap.get(this.#state).classList.toggle("on", on);
+  private change(on: boolean) {
+    this.stateToDivMap.get(this.state).classList.toggle("on", on);
   }
 }
 
